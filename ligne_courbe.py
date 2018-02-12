31acff490d219e43c1f68907d4c5fbfd42cdc15f
Hii-Chan <hichma.k@hotmail.fr>
2018-02-12 12:49:23 +0100

    Présentation, majuscule...

--- ligne_courbe.py
+++ ligne_courbe.py
@@ -1,27 +1,31 @@
 import time
 
 
+def tourner(Vg, Vd):
+    pass
+
+
 def SuivreLigneDroite(capteurD, capteurC, capteurG, K, Vd, Vg):
-	while capteurC == 1:
-		While True :
-		If capteurD == 1 and CapteurG == 0:
-			T = time.clock()
-			While CapteurD == 1 : 
-				t = time.clock()
-				Tourner (Vg, Vd - K(t - T)) /(Vg, -Vd)
-				CapteurD = input(CapteurD)
-                
-		Elif capteurG == 1 and CapteurG == 0:
-			T = time.clock()
-			While CapteurG == 1:
-				t = time.clock()
-				Tourner (Vg - K(t - T), Vd) /(Vg, -Vd)
-				CapteurG = input(CapteurG)
-                
-		Elif capteurD == 0 and capteurG == 0:
-			Tourner (Vg,Vd)
-			CapteurG = input(CapteurG)
-			CapteurD = input(CapteurD)
-            
-		Else :
-			Break
+    while capteurC == 1:
+        while True:
+            if capteurD == 1 and CapteurG == 0:
+                T = time.clock()
+                while CapteurD == 1:
+                    t = time.clock()
+                    tourner(Vg, Vd - K(t - T)) / (Vg, -Vd)
+                    CapteurD = input(CapteurD)
+
+            elif capteurG == 1 and CapteurG == 0:
+                T = time.clock()
+                while CapteurG == 1:
+                    t = time.clock()
+                    tourner(Vg - K(t - T), Vd) / (Vg, -Vd)
+                    CapteurG = input(CapteurG)
+
+            elif capteurD == 0 and capteurG == 0:
+                tourner(Vg, Vd)
+                CapteurG = input(CapteurG)
+                CapteurD = input(CapteurD)
+
+            else:
+                break
--- ligne_huit.py
+++ ligne_huit.py
@@ -1,18 +1,19 @@
-from ligne_courbe.py import *
+def tourner(Vg, Vd):
+    pass
+
 
 def SuivreLigneHuit(capteurG, capteurC, capteurD, K, Vg, Vd, choix):
-	while True:
-		SuivreLigneDroite(capteurD, capteurC, capteurG, K, Vd, Vg)
-     	k = 0
+    while True:
+        SuivreLigneDroite(capteurD, capteurC, capteurG, K, Vd, Vg)
+        k = 0
         while k != 1 or capteurC != 1:
-       		if choix == 'gauche' :
-            	tourner(-Vg, Vd)
+            if choix == 'gauche' :
+                tourner(-Vg, Vd)
             elif choix == 'droite' :
-            	tourner(Vg, -Vd)
+                tourner(Vg, -Vd)
             else :
-            	tourner(Vg, Vd)
-                
-           capteurC == input(capteurC)
-           
-           if capteurC == 0:
-                	k = 1
+                tourner(Vg, Vd)
+            capteurC == input(capteurC)
+
+            if capteurC == 0:
+                k = 1
