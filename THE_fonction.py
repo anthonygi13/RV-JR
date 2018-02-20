c131367f38c4a814a5150a02a1625b11c057ffc6
Hii-Chan <hichma.k@hotmail.fr>
2018-02-20 16:32:36 +0100

    THE function
    writing of the great function

--- THE_fonction.py
+++ THE_fonction.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 
 def controle_moteur (VitesseG, VitesseD):
     pass
-gauche = 1 #numéro des pins à vérifier
+gauche = 1 #numéro des pins à établir parce que on sait que ça sera pas ça
 droite = 2
 centre = 3
 
@@ -13,55 +13,60 @@ GPIO.setup(gauche,GPIO.IN)
 GPIO.setup(droite, GPIO.IN)
 GPIO.setup(centre, GPIO.IN)
 
-def SuivreLigne (capteurG, capteurC, capteurD, constante, VitesseD, VitesseG, choix):
+
+def SuivreLigne (constante, VitesseD, VitesseG, choix):
+    capteurG = GPIO.input(gauche)
+    capteurD = GPIO.input(droite)
+    capteurC = GPIO.input(centre)
     while True: #boucle infinie, on n'a pas de cas terminal pour l'instant (à voir plus tard...)
         while capteurC == GPIO.HIGH:
             if capteurD == GPIO.HIGH and capteurG == GPIO.LOW: #tourner à droite
                 T = time.clock()
-                while capteurD == 1:
+                while capteurD == GPIO.HIGH:
                     t = time.clock()
                     controle_moteur(VitesseG, VitesseD - constante * (t - T))
-                    capteurD = input(capteurD)
+                    capteurD = GPIO.input(droite)
 
-            elif capteurG == 1 and capteurG == 0: #tourner à gauche
+            elif capteurG == GPIO.HIGH and capteurG == GPIO.LOW: #tourner à gauche
                 T = time.clock()
-                while capteurG == 1:
+                while capteurG == GPIO.HIGH:
                     t = time.clock()
                     controle_moteur(VitesseG - constante * (t - T), VitesseD)
-                    capteurG = input(capteurG)
+                    capteurG = GPIO.input(gauche)
 
-            elif capteurD == 0 and capteurG == 0: #continuer tout droit
+            elif capteurD == GPIO.LOW and capteurG == GPIO.LOW: #continuer tout droit
                 controle_moteur(VitesseG, VitesseD)
-                capteurG = input(capteurG)
-                capteurD = input(capteurD)
+                capteurG = GPIO.input(gauche)
+                capteurD = GPIO.input(droite)
+                capteurC = GPIO.input(centre)
 
-            else:
+            else: #intersection = traité plus bas
                 break
 
         # tous les capteurs sont dans le noir = intersection
-        #choix = choix de là où le capteur tourne = 'gauche', 'droite' ou 'centre'
-        if capteurD == 1 and capteurC == 1 :
+        #choix = choix de là où le robot tourne = 'gauche', 'droite' ou 'centre'
+        if capteurD == GPIO.HIGH and capteurC == GPIO.HIGH :
             est_passe_blanc = False #variable permettant de savoir si le capteur centrale est déjà passé dans le blanc ou pas
-            while not est_passe_blanc or capteurC != 1: #la boucle s'arrête lorsque le capteur revient dans le noir après être passé par le blanc
+            while not est_passe_blanc or capteurC != GPIO.HIGH: #la boucle s'arrête lorsque le capteur revient dans le noir après être passé par le blanc
                 if choix == 'gauche':
                     controle_moteur(-VitesseG, VitesseD)
                 elif choix == 'droite':
                     controle_moteur(VitesseG, -VitesseD)
                 else:
                     controle_moteur(VitesseG, VitesseD)
-                    capteurC == input(capteurC)
+                    capteurC = GPIO.input(centre)
 
-                if capteurC == 0:
+                if capteurC == GPIO.LOW:
                     est_passe_blanc = True
 
 
-        #tous les capteurs sont dans le blanc = demi-tour
-        while capteurC != 1:
+        #tous les capteurs sont dans le blanc => demi-tour
+        while capteurC != GPIO.HIGH:
             if choix == 'gauche' or choix == 'centre':
                 controle_moteur(-VitesseG, VitesseD)
             else:
                 controle_moteur(VitesseG, -VitesseD)
-                capteurC = input(capteurC)
+                capteurC = GPIO.input(centre)
 
 
 
