b481a119db5046304212db88f1e241bd14092740
Hii-Chan <hichma.k@hotmail.fr>
2018-02-20 14:29:58 +0100

    Auto stash before merge of "master" and "origin/master"

--- simulateur/THE_fonction.py
+++ simulateur/THE_fonction.py
@@ -1,14 +1,6 @@
-import time
 
-# Va falloir se mettre d accord sur la structure generale de notre code
-# je pensais que l utilisation de cette fonction serais un truc du style while (y a pas d'intersection): suivrelignedroite(input(capteurD), input(capteurC), input(capteurG), K, Vd, Vg)
-# mais la c pas compatible
-# quand un variable vaut 1 ou 0 mettez plutot des booleens
-# utilisez des noms de variables clairs, c pas evident t et T...
-
-def tourner(Vg, Vd):
+def SuivreLigne (capteurG, capteurC, capteurD, constante, VitesseD, VitesseG):
     pass
-
 
 def SuivreLigneDroite(capteurD, capteurC, capteurG, K, Vd, Vg):
     while capteurC == 1:
