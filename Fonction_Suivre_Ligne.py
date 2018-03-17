b2be492141285335b63a834cb81a7492df51c035
Kilian <kilian.macdonald@hotmail.fr>
2018-03-17 14:24:00 +0100

    Code avec les 5 capteurs

--- Fonction_Suivre_Ligne.py
+++ Fonction_Suivre_Ligne.py
@@ -12,14 +12,19 @@ from motor_control import *
 
 #DEFITION DES PARAMETRES (valeurs à rentrer ici)
 
-gauche = 0  # numero des pins a determiner et a changer !
-droite = 0
+arriere_gauche = 0 # numero des pins a determiner et a changer !
+gauche = 0
 centre = 0
+droite = 0
+arriere_droite = 0
 
 GPIO.setmode(GPIO.BOARD)
+capteurAG = GPIO.setup(arriere_gauche, GPIO.IN)
 capteurG = GPIO.setup(gauche, GPIO.IN)
-capteurD = GPIO.setup(droite, GPIO.IN)
 capteurC = GPIO.setup(centre, GPIO.IN)
+capteurD = GPIO.setup(droite, GPIO.IN)
+capteurAD = GPIO.setup(arriere_droite, GPIO.IN)
+
 
 noir = GPIO.LOW
 blanc = GPIO.HIGH
@@ -38,12 +43,19 @@ blanc = GPIO.HIGH
 # Structure du programme à modifier avec les differents cas
 
 def actualise_capteurs(capteurG, capteurC, capteurD):
-    capteurG = GPIO.input()  # Faudra mettre le numéro des pins des capteurs correpondant dans les parenthèses
+    capteurAG = GPIO.input() # Faudra mettre le numéro des pins des capteurs correpondant dans les parenthèses
+    capteurG = GPIO.input()
     capteurC = GPIO.input()
     capteurD = GPIO.input()
+    capteurAD = GPIO.input()
 
-def est_dans_le_noir(capteur):
+
+def est_dans_le_noir (capteur):
     return capteur == noir
+
+
+def tout_a_disparu():
+    return capteurAG == noir and capteurG == noir and capteurC == noir and capteurD == noir and capteurAD == noir
 
 
 def avance(VitesseG, VitesseD):
@@ -67,13 +79,17 @@ def demi_tour(VitesseG, VitesseD, choix):
 
 # Pareil, faudra mettre le numéro des pins des capteurs à la place des '??', chaque pin dans chaque cas
 # (par exemple) :
+GPIO.add_event_detect('??', GPIO.RISING, callback=actualise_capteurs)  # pin de capteurAG
 GPIO.add_event_detect('??', GPIO.RISING, callback=actualise_capteurs)  # pin de capteurG
 GPIO.add_event_detect('??', GPIO.RISING, callback=actualise_capteurs)  # pin de capteurC
 GPIO.add_event_detect('??', GPIO.RISING, callback=actualise_capteurs)  # pin de capteurD
+GPIO.add_event_detect('??', GPIO.RISING, callback=actualise_capteurs)  # pin de capteurAD
 
+GPIO.add_event_detect('??', GPIO.FALLING, callback=actualise_capteurs)  # pin de capteurAG
 GPIO.add_event_detect('??', GPIO.FALLING, callback=actualise_capteurs)  # pin de capteurG
 GPIO.add_event_detect('??', GPIO.FALLING, callback=actualise_capteurs)  # pin de capteurC
 GPIO.add_event_detect('??', GPIO.FALLING, callback=actualise_capteurs)  # pin de capteurD
+GPIO.add_event_detect('??', GPIO.FALLING, callback=actualise_capteurs)  # pin de capteurAD
 
 
 def intersection(VitesseG, VitesseD, choix):  # le choix peut aussi être 'centre', auquel cas le robot ira en face
@@ -91,10 +107,8 @@ def intersection(VitesseG, VitesseD, choix):  # le choix peut aussi être 'centr
 # parametrer capteur blanc et capteur noir
 
 def SuivreLigne(constante, VitesseD, VitesseG, choix):  # il va falloir modifier les cas non ?
-    global capteurG, capteurC, capteurD
-    actualise_capteurs(capteurG, capteurC, capteurD)
 
-    while True:  # boucle infinie, on n'a pas de cas terminal pour l'instant (à voir plus tard...) STOP AVEC CLAVIER
+    while not tout_a_disparu():  # boucle infinie, on n'a pas de cas terminal pour l'instant (à voir plus tard...) STOP AVEC CLAVIER <-- On a un cas terminal
         if pygame.event.type == pygame.KEYDOWN:
             if pygame.event.key == pygame.K_s:
                 controle_moteur(0, 0)
@@ -116,3 +130,5 @@ def SuivreLigne(constante, VitesseD, VitesseG, choix):  # il va falloir modifier
             demi_tour(VitesseG, VitesseD, choix)
 
         actualise_capteurs(capteurG, capteurC, capteurD)
+
+    # Vous pensez qu'on pourrai faire clignoter une LED pour dire que le robot a fini son parcour ? ça peut être stylé
