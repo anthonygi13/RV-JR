453accb3f4c2a87e92a1df6f6af0c6df9caa667a
Unknown <anthony.giraudo@etu.univ-amu.fr>
2018-04-04 18:43:54 +0200

    ça marche !!!!!

--- programme_suiveur_de_ligne/classes.py
+++ programme_suiveur_de_ligne/classes.py
@@ -1,17 +1,14 @@
-# Author: Giraudo Anthony                     
+# Author: Giraudo Anthony, Kari Hichma, Marinho Louise, kilian Mac Donald
 # 15 mars 2018
 # classes.py
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-
-
 class Capteur:
     def __init__(self, gpio):
         self.gpio = gpio
         GPIO.setup(gpio, GPIO.IN)
-	self.etat = GPIO.input(gpio)
+        self.etat = GPIO.input(gpio)
 
     def actualiser_etat(self):
         self.etat = GPIO.input(self.gpio)
@@ -22,7 +19,7 @@ class Capteur:
 class Robot:
     #(l-d)/2l eventuellement rajouter en parametre
 
-    def __init__(self, gpio_capteur_centre, gpio_capteur_gauche, gpio_capteur_droit, vitesse_de_marche, inverser_sens_moteur_gauche, inverser_sens_moteur_droit, coeff):
+    def __init__(self, gpio_capteur_centre, gpio_capteur_gauche, gpio_capteur_droit, vitesse_de_marche, inverser_sens_moteur_gauche, inverser_sens_moteur_droit, coeff, gpio_pwm_roue_gauche, gpio_pwm_roue_droite):
         self.capteur_centre = Capteur(gpio_capteur_centre)
         self.capteur_droit = Capteur(gpio_capteur_droit)
         self.capteur_gauche = Capteur(gpio_capteur_gauche)
@@ -32,6 +29,15 @@ class Robot:
         self.inverser_sens_moteur_droit = inverser_sens_moteur_droit
         self.inverser_sens_moteur_gauche = inverser_sens_moteur_gauche
         self.coeff = coeff
+        self.gpio_pwm_roue_gauche = gpio_pwm_roue_gauche
+        self.gpio_pwm_roue_droite = gpio_pwm_roue_droite
+        GPIO.setup(gpio_pwm_roue_droite, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(gpio_pwm_roue_gauche, GPIO.OUT, initial=GPIO.LOW)
+        self.pwm_roue_droite = GPIO.PWM(gpio_pwm_roue_droite, 8)
+        self.pwm_roue_gauche = GPIO.PWM(gpio_pwm_roue_gauche, 8)
+        self.pwm_roue_droite.start(0)
+        self.pwm_roue_gauche.start(0)
+
 
     def controle_moteur(self, vitesse_gauche, vitesse_droite):
         """
@@ -42,6 +48,7 @@ class Robot:
         B droite
         "-" correspond au sens "arriere"
         """
+
         if self.inverser_sens_moteur_gauche:
             vitesse_gauche = -vitesse_gauche
         if self.inverser_sens_moteur_droit:
@@ -50,41 +57,35 @@ class Robot:
         self.vitesse_droite = vitesse_droite
         self.vitesse_gauche = vitesse_gauche
 
-        ENA = 16
-        ENB = 18
-        IN1 = 29
-        IN2 = 31
-        IN3 = 33
-        IN4 = 35
+        ENA = self.gpio_pwm_roue_droite
+        ENB = self.gpio_pwm_roue_gauche
+        IN1 = 32
+        IN2 = 36
+        IN3 = 40
+        IN4 = 37
 
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(ENA, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(ENB, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(IN1, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(IN2, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(IN3, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(IN4, GPIO.OUT, initial=GPIO.LOW)
 
-        pwm_A = GPIO.PWM(ENA, 8)  # frequence a determiner
-        pwm_B = GPIO.PWM(ENB, 8)
-
         if vitesse_gauche > 0:
             GPIO.output(IN1, GPIO.HIGH)  # rotation sens horaire
             GPIO.output(IN2, GPIO.LOW)
-            pwm_A.start(abs(vitesse_gauche))
+            self.pwm_roue_gauche.ChangeDutyCycle(abs(vitesse_gauche))
         elif vitesse_gauche < 0:
             GPIO.output(IN1, GPIO.LOW)  # anti horaire
             GPIO.output(IN2, GPIO.HIGH)
-            pwm_A.start(abs(vitesse_gauche))
+            self.pwm_roue_gauche.ChangeDutyCycle(abs(vitesse_gauche))
 
         if vitesse_droite > 0:
             GPIO.output(IN3, GPIO.HIGH)  # rotation sens horaire
             GPIO.output(IN4, GPIO.LOW)
-            pwm_B.start(abs(vitesse_droite))
+            self.pwm_roue_droite.ChangeDutyCycle(abs(vitesse_droite))
         elif vitesse_droite < 0:
             GPIO.output(IN3, GPIO.LOW)  # anti horaire
             GPIO.output(IN4, GPIO.HIGH)
-            pwm_B.start(abs(vitesse_droite))
+            self.pwm_roue_droite.ChangeDutyCycle(abs(vitesse_droite))
 
     def tourner_gauche(self):
         self.controle_moteur(0, self.vitesse * self.coeff)
@@ -122,4 +123,3 @@ class Robot:
         self.capteur_centre.actualiser_etat()
         self.capteur_gauche.actualiser_etat()
         self.capteur_droit.actualiser_etat()
-
--- programme_suiveur_de_ligne/main_loop.py
+++ programme_suiveur_de_ligne/main_loop.py
@@ -1,4 +1,4 @@
-# Author: Giraudo Anthony                     
+# Author: Giraudo Anthony, Kari Hichma, Marinho Louise, Kilian Mac Donald
 # 15 mars 2018
 # main_loop.py
 
@@ -12,22 +12,24 @@ GPIO.setmode(GPIO.BOARD)
 
 from classes import *
 
-coeff = 100
-gpio_capteur_centre = 40
-gpio_capteur_droit = 38
-gpio_capteur_gauche = 36
-vitesse_de_marche = 1
+coeff = 1
+gpio_capteur_centre = 11
+gpio_capteur_droit = 13
+gpio_capteur_gauche = 15
+vitesse_de_marche = 100
 inverser_sens_moteur_droit = False
 inverser_sens_moteur_gauche = False
 choix_1 = "droite"
 choix_2 = None
+gpio_pwm_roue_droite = 16
+gpio_pwm_roue_gauche = 12
 
-robot = Robot(gpio_capteur_centre, gpio_capteur_gauche, gpio_capteur_droit, vitesse_de_marche, inverser_sens_moteur_droit, inverser_sens_moteur_gauche, coeff)
 
-GPIO.add_event_detect(gpio_capteur_centre, GPIO.RISING, callback=robot.actualiser_etat_capteurs(), bouncetime =20)
-GPIO.add_event_detect(gpio_capteur_droit, GPIO.RISING, callback=robot.actualiser_etat_capteurs(), bouncetime =20)
-GPIO.add_event_detect(gpio_capteur_gauche, GPIO.RISING, callback=robot.actualiser_etat_capteurs(), bouncetime =20)
+robot = Robot(gpio_capteur_centre, gpio_capteur_gauche, gpio_capteur_droit, vitesse_de_marche, inverser_sens_moteur_droit, inverser_sens_moteur_gauche, coeff, gpio_pwm_roue_gauche, gpio_pwm_roue_droite)
 
+GPIO.add_event_detect(gpio_capteur_centre, GPIO.RISING, callback=lambda x: robot.actualiser_etat_capteurs(), bouncetime =20)
+GPIO.add_event_detect(gpio_capteur_droit, GPIO.RISING, callback=lambda x: robot.actualiser_etat_capteurs(), bouncetime =20)
+GPIO.add_event_detect(gpio_capteur_gauche, GPIO.RISING, callback=lambda x: robot.actualiser_etat_capteurs(), bouncetime =20)
 
 while True:
     if pygame.event.type == pygame.KEYDOWN:
@@ -50,3 +52,5 @@ while True:
 
     else:
         robot.tout_droit()
+
+GPIO.cleanup()
